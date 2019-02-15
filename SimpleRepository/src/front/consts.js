3fb284c8f4a1ecae5cf35f558249f65a13291dae
yitakura731 <yitakura731@gmail.com>
2019-02-15 16:30:07 +0900

    Delete Unnecessary Directory

--- SimpleRepository/src/front/consts.js
+++
@@ -1,5 +0,0 @@
-const Consts = {
-  HEADER_HEIGHT: 54,
-  CONTENTS_HEIGHT: window.innerHeight - 54
-};
-export default Consts;
--- SimpleRepository/src/front/event-bus.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue';
-
-export const DialogEvent = new Vue();
