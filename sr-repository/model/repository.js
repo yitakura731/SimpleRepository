e26010226674c5c21cebf160a4a9a78b4e1829a7
yitakura731 <yitakura731@gmail.com>
2020-01-27 13:42:34 +0900

    show jpeg thumbnail error

--- sr-repository/model/repository.js
+++ sr-repository/model/repository.js
@@ -51,12 +51,16 @@ module.exports = class Repository {
     let thumbnailName = null;
     if (file.mimetype === 'image/jpeg') {
       thumbnailName = `${puid.generate()}.jpg`;
-      const thumbnai = await Jimp.read(path.join(this.docFileRoot, filename));
-      logger.debug(`postDocument: read jpeg thumnail file ${thumbnailName}`);
-      await thumbnai.resize(100, 100);
-      logger.debug(`postDocument: resize jpeg thumnail file`);
-      await thumbnai.write(path.join(this.docFileRoot, thumbnailName));
-      logger.debug('postDocument: end store jpeg thumnail file');
+      try {
+        const thumbnai = await Jimp.read(path.join(this.docFileRoot, filename));
+        logger.debug(`postDocument: read jpeg thumnail file ${thumbnailName}`);
+        await thumbnai.resize(100, 100);
+        logger.debug(`postDocument: resize jpeg thumnail file`);
+        await thumbnai.write(path.join(this.docFileRoot, thumbnailName));
+        logger.debug('postDocument: end store jpeg thumnail file');
+      } catch (err) {
+        logger.debug(err);
+      }
     } else {
       thumbnailName = 'pdfNail.png';
     }
